d2ac9a3bfa8dbc18da98a9607bb9a1d920a1476d
Papuna Fshaveli <papunafshaveli@gmail.com>
2023-10-09 21:47:33 +0400

    edit comment

--- src/pages/Planet.jsx
+++ src/pages/Planet.jsx
@@ -23,8 +23,8 @@ export default function Planet() {
           </Wikipedia>
         </Source>
       </PlanetNameAndInfo>
-      {/* PlanetNameAndInfo და მართკუთხა ინფოები მოვაქციო ერთ დივში და გავფლექსო */}
-      ჰორიზონტალურად
+      {/* PlanetNameAndInfo და მართკუთხა ინფოები მოვაქციო ერთ დივში და გავფლექსო 
+      ჰორიზონტალურად */}
     </PlanetDiv>
   );
 }
